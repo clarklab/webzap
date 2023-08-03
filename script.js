1224fc98c3d6a5067a2ecc6b8e0c3781f44caa81
Clark Wimberly <clark@clarklab.net>
2023-08-03 01:58:46 -0500

    type

--- script.js
+++ script.js
@@ -18,3 +18,31 @@
         } else {
             console.error('Search form element not found.');
         }
+
+
+        function simulateTyping() {
+            const inputElement = document.getElementById('searchQuery');
+            const query = "PERSIAN MYTHOLOGY";
+            const typingSpeed = 40; // Speed in milliseconds per character
+            const maxRandomDelay = 400; // Maximum additional random delay in milliseconds
+
+            let currentIndex = 0;
+
+            function typeCharacter() {
+                inputElement.value = query.slice(0, currentIndex + 1);
+
+                if (currentIndex < query.length - 1) {
+                    const randomDelay = Math.floor(Math.random() * maxRandomDelay);
+                    currentIndex++;
+                    setTimeout(typeCharacter, typingSpeed + randomDelay);
+                }
+            }
+
+            typeCharacter();
+        }
+
+        // Trigger the typing simulation after a 2-second delay
+        window.onload = function() {
+            setTimeout(simulateTyping, 2000); // 2-second delay (2000 milliseconds)
+        };
+
